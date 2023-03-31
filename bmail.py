a14ae1d55d1c9e8c278ed9c6e4e62f4e7ebc65b0
Thanmayee J <92224593+thanmayee-29@users.noreply.github.com>
2023-03-31 23:56:33 +0530

    Update bmail.py

--- bmail.py
+++ bmail.py
@@ -1,27 +1,43 @@
 import pandas as pd
 from email.message import EmailMessage
-from email.mime.text import MIMEText #only text based-content
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 import smtplib
 from datetime import datetime
+from email import encoders
 
 
-df = pd.read_excel('your_bday_list1.xlsx')
+df = pd.read_excel('your_bdays_list.xlsx')
 today = datetime.now().date()
 today_bdays = df[df['birthday'].dt.date == today]
-
+msg = MIMEMultipart()
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login('enter your email', 'Authorized-app-password')
-#the Authorized-app-password can be generated in the gmail-account-manage-->security-->enable 2step verification-->App passwords-->select app and device-->generate
+server.login('janapareddythanmayee29@gmail.com', 'ygqyjjuntjqlbjkt')
 
 for index, row in today_bdays.iterrows():
     name = row['name']
     email = row['email']
-    message = f'{name} your birthday message'
-    msg = MIMEText(message)
-    msg['Subject'] = 'the appropriate subject accordingly'
-    msg['From'] = 'enter your email'
-    msg['To'] = email
-    server.sendmail('enter your email', email, msg.as_string())
+
+    text = f'{name} Thanu hopes that we can always be together on this life-adventure. You know that Thanu will always be your best of friends right!!? and so HAPPY BIRTHDAY {name}!! May all your wishes come true.'
+    part1 = MIMEText(text, "plain")
+    msg.attach(part1)
+    with open(r'C:\Users\yowaisquad\OneDrive\Documents\cake ig.png', 'rb') as f:
+        image_data = f.read()
+    image = MIMEBase(
+        'image', 'png', filename=r'C:\Users\yowaisquad\OneDrive\Documents\cake ig.png')
+    image.set_payload(image_data)
+    encoders.encode_base64(image)
+    image.add_header('Content-Disposition', 'attachment',
+                     filename=r'C:\Users\yowaisquad\OneDrive\Documents\cake ig.png')
+    msg.attach(image)
+
+msg['Subject'] = 'Look who\'s shining on their glorious day!!'
+msg['From'] = 'janapareddythanmayee29@gmail.com'
+msg['To'] = email
+server.sendmail('janapareddythanmayee29@gmail.com', email, msg.as_string())
+
 
 server.quit()
